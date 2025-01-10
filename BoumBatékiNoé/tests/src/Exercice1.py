871393a0b832cbd8279bb9042dd171c559a899f2
noe-bbat <n.boumbateki@gmail.com>
2025-01-10 16:06:23 +0100

    Implémentation de la fonction Premier ou Pas(échec des tests)

--- BoumBatékiNoé/tests/src/Exercice1.py
+++ BoumBatékiNoé/tests/src/Exercice1.py
@@ -1,3 +1,4 @@
+import math
 class Exercice1:
 	@staticmethod
 	def TroisGrandNombre(numbers: list[int]) -> list[int]:
@@ -5,7 +6,12 @@ class Exercice1:
 	
 	@staticmethod
 	def PremierOuNon(number: int) -> bool:
-		pass
+		if number == 1: #Execption 
+			return True
+		for i in range(2, math.ceil(number**0.5)):
+			if number % i == 0:
+				return False
+		return True
 	
 	@staticmethod
 	def ArithmeOuPas(numbers: list[int]) -> bool:
