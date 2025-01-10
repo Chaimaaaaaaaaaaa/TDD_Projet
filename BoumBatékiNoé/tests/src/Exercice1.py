873b9ede0d046743995f2c994754f9d22ff612db
noe-bbat <n.boumbateki@gmail.com>
2025-01-10 15:40:56 +0100

    Prototype des fonctions de l'exercice 1

---
+++ BoumBatékiNoé/tests/src/Exercice1.py
@@ -0,0 +1,9 @@
+class Exercice1:
+	def TroisGrandNombre(numbers: list[int]) -> list[int]:
+		pass
+	
+	def PremierOuNon(number: int) -> bool:
+		pass
+	
+	def ArithmeOuPas(numbers: list[int]) -> bool:
+		pass
