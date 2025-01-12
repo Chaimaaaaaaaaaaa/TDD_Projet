d19fcda93e142870ae6c2ddb7a9e361ee5c59593
noe-bbat <n.boumbateki@gmail.com>
2025-01-12 21:14:37 +0100

    Refactorisation du code

--- BoumBatékiNoé/tests/src/Exercice3.py
+++ BoumBatékiNoé/tests/src/Exercice3.py
@@ -1,135 +1,53 @@
-# source : https://github.com/lamchau/refactoring-exercise/blob/master/python/funding_raised.py
 import csv
 
 class FundingRaised:
-  @staticmethod
-  def where(options = {}):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+    @staticmethod
+    def _read_csv(file_path):
+        with open(file_path, "rt") as csvfile:
+            data = csv.reader(csvfile, delimiter=',', quotechar='"')
+            next(data)  
+            return [row for row in data]
 
-    funding = []
-    if 'company_name' in options:
-      result = []
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          result.append(row)
-      csv_data = result
+    @staticmethod
+    def _filter_data(data, filters):
+        for key, value in filters.items():
+            if key not in ['company_name', 'city', 'state', 'round']:
+                continue
+            index = {
+                'company_name': 1,
+                'city': 4,
+                'state': 5,
+                'round': 9
+            }.get(key)
+            data = [row for row in data if row[index] == value]
+        return data
 
-    if 'city' in options:
-      result = []
-      for row in csv_data:
-        if row[4] == options['city']:
-          result.append(row)
-      csv_data = result
+    @staticmethod
+    def _map_row(row):
+        return {
+            'permalink': row[0],
+            'company_name': row[1],
+            'number_employees': row[2],
+            'category': row[3],
+            'city': row[4],
+            'state': row[5],
+            'funded_date': row[6],
+            'raised_amount': row[7],
+            'raised_currency': row[8],
+            'round': row[9]
+        }
 
-    if 'state' in options:
-      result = []
-      for row in csv_data:
-        if row[5] == options['state']:
-          result.append(row)
-      csv_data = result
+    @staticmethod
+    def where(options=None):
+        options = options or {}
+        csv_data = FundingRaised._read_csv("../startup_funding.csv")
+        filtered_data = FundingRaised._filter_data(csv_data, options)
+        return [FundingRaised._map_row(row) for row in filtered_data]
 
-    if 'round' in options:
-      result = []
-      for row in csv_data:
-        if row[9] == options['round']:
-          result.append(row)
-      csv_data = result
-
-    output = []
-    for row in csv_data:
-      mapped = {}
-      mapped['permalink'] = row[0]
-      mapped['company_name'] = row[1]
-      mapped['number_employees'] = row[2]
-      mapped['category'] = row[3]
-      mapped['city'] = row[4]
-      mapped['state'] = row[5]
-      mapped['funded_date'] = row[6]
-      mapped['raised_amount'] = row[7]
-      mapped['raised_currency'] = row[8]
-      mapped['round'] = row[9]
-      output.append(mapped)
-
-    return output
-
-  @staticmethod
-  def find_by(options):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
-
-    if 'company_name' in options:
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'city' in options:
-      for row in csv_data:
-        if row[4] == options['city']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'state' in options:
-      for row in csv_data:
-        if row[5] == options['state']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'round' in options:
-      for row in csv_data:
-        if row[9] == options['round']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    raise RecordNotFound
+    @staticmethod
+    def find_by(options):
+        csv_data = FundingRaised._read_csv("../startup_funding.csv")
+        filtered_data = FundingRaised._filter_data(csv_data, options)
+        if filtered_data:
+            return FundingRaised._map_row(filtered_data[0])
+        raise RecordNotFound
