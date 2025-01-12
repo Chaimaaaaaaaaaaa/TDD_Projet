2dfdd52406bd64c7d8231745ba56867259458275
noe-bbat <n.boumbateki@gmail.com>
2025-01-12 17:00:19 +0100

    Implémentation fonctions LIFO(Test réussis)

--- BoumBatékiNoé/tests/src/Exercice2.py
+++ BoumBatékiNoé/tests/src/Exercice2.py
@@ -9,3 +9,13 @@ class FIFO:
 		self.queue.put(x)
 	def minus(self):
 		return self.queue.get()
+
+class LIFO:
+	def __init__(self):
+		self.queue = queue.LifoQueue()
+	def taille(self):
+		return self.queue.qsize()
+	def plus(self, x:int):
+		self.queue.put(x)
+	def minus(self):
+		return self.queue.get()
