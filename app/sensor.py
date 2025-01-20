284cca317a7fa738915cf989f52dd50d6bff582a
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2025-01-20 15:09:27 +0100

    Ajout d'une fonction qui va permettre la conversion des données du capteur et tests correspondant

--- app/sensor.py
+++ app/sensor.py
@@ -1,6 +1,12 @@
+import smbus2
+import time
 
 class MPL115A2:
     def __init__(self, i2c_address=0x60, bus_num=1):
+        self.address = i2c_address
+        self.bus = smbus2.SMBus(bus_num)
+
+    def start_conversion(self):
         pass
 
     def read_data(self):
--- app/test_sensor.py
+++ app/test_sensor.py
@@ -4,6 +4,13 @@ from sensor import MPL115A2
 
 class TestMPL115A2(unittest.TestCase):
     @patch('smbus2.SMBus')
+    def test_start_conversion(self, MockSMBus):
+        mock_bus = MockSMBus.return_value
+        sensor = MPL115A2()
+        sensor.start_conversion()
+        mock_bus.write_byte_data.assert_called_with(0x60, 0x12, 0x00)
+
+    @patch('smbus2.SMBus')
     def test_read_data(self, MockSMBus):
         mock_bus = MockSMBus.return_value
 
