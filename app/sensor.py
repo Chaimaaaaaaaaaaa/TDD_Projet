c2fa1250c16644458d040863c8c8437ea863948c
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2025-01-20 11:15:51 +0100

    Premiers tests

---
+++ app/sensor.py
@@ -0,0 +1,10 @@
+
+class MPL115A2:
+    def __init__(self, i2c_address=0x60, bus_num=1):
+        pass
+
+    def read_data(self):
+        pass
+
+    def write_register(self, register, value):
+        pass
---
+++ app/test_sensor.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import patch
+from sensor import MPL115A2
+
+class TestMPL115A2(unittest.TestCase):
+    @patch('smbus2.SMBus')
+    def test_read_data(self, MockSMBus):
+        mock_bus = MockSMBus.return_value
+
+        mock_bus.read_word_data.side_effect = [
+            0x8000,
+            0x4000
+        ]
+
+        sensor = MPL115A2()
+        data = sensor.read_data()
+
+        self.assertEqual(data['pressure'], 0x0080)
+        self.assertEqual(data['temperature'], 0x0040)
+
+    @patch('smbus2.SMBus')
+    def test_write_register(self, MockSMBus):
+
+        register = 0x12
+        value = 0x55
+        sensor.write_register(register, value)
+
+if __name__ == '__main__':
+    unittest.main()
