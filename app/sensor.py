969cb87db6d497fa3c6dffc4af35ebcf727548ce
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2025-01-28 11:14:24 +0100

    Ajout des coefficients de calibration dans la fonction d'initialisation.

--- app/sensor.py
+++ app/sensor.py
@@ -5,6 +5,12 @@ class MPL115A2:
     def __init__(self, bus=1, address=0x60):
         self.bus = smbus2.SMBus(bus)  
         self.address = address
+        #Coefficients de calibration stockés dans le capteur.
+        self.a0 = None
+        self.b1 = None
+        self.b2 = None
+        self.c12 = None
+        self.read_coefficients()
 
     def read_coefficients(self):
         data = self.bus.read_i2c_block_data(self.address, 0x04, 8)
@@ -13,9 +19,6 @@ class MPL115A2:
         self.b1 = (data[2] << 8 | data[3]) / 8192.0
         self.b2 = (data[4] << 8 | data[5]) / 16384.0
         self.c12 = ((data[6] << 8 | data[7]) >> 2) / 4194304.0
-
-    def read_raw_data(self):
-        pass
-
+        
     def get_pressure_temperature(self):
         pass
