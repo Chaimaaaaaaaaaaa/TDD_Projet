bc4754e8cf3e46d3ab8b797cc53d5268dc9816ce
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2025-01-28 10:49:56 +0100

    Fonction d'initialisation

--- app/sensor.py
+++ app/sensor.py
@@ -3,9 +3,13 @@ from time import sleep
 
 class MPL115A2:
     def __init__(self, bus=1, address=0x60):
-        pass
+        self.bus = smbus2.SMBus(bus)  
+        self.address = address
 
     def read_coefficients(self):
+        pass
+
+    def read_raw_data(self):
         pass
 
     def get_pressure_temperature(self):
